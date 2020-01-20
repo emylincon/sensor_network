fbf652330d7301e6d2166cfd1a8ac7fa227f709a
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-20 23:40:02 +0000

    server node done

--- server.py
+++ server.py
@@ -5,18 +5,61 @@ import json
 import os
 import matplotlib.pyplot as plt
 from drawnow import *
+import paho.mqtt.client as mqtt
+import os
 
-
-plot_data = {}
-data_arr = []
-thread_list = []
+#192.168.40.132
 fig = plt.figure()
-ax1 = fig.add_subplot(221)
-ax2 = fig.add_subplot(222)
-ax3 = fig.add_subplot(223)
-ax4 = fig.add_subplot(224)
+data_dict = {}     # {temp_server:{Temperature:[], Memory:[], CPU:[]}, humidity_server:{Humidity:[], Memory:[], CPU:[]}}
 
-data_dict = {}
+
+def start_up():
+    global username, password, broker_ip, topic, broker_port_no
+    os.system('clear')
+    print('-----------------------------------')
+    print('Welcome to MQTT Subscriber client')
+    print('-----------------------------------')
+
+    username = input('Username of Broker: ').strip()
+    password = input('Password of Broker: ').strip()
+    broker_ip = input("Broker's IP: ").strip()
+    broker_port_no = 1883
+    topic = input("Topic: ").strip()   # topic layering use iot/#
+    print('-----------------------------------')
+    br = th.Thread(target=broker_loop())
+    br.start()
+
+
+# Callback Function on Connection with MQTT Server
+def on_connect(connect_client, userdata, flags, rc):
+    print("Connected with Code :" +str(rc))
+    # Subscribe Topic from here
+    connect_client.subscribe(topic)
+
+
+# Callback Function on Receiving the Subscribed Topic/Message
+def on_message(message_client, userdata, msg):
+    # print the message received from the subscribed topic
+    data = str(msg.payload, 'utf-8').split()
+    topic = msg.topic
+    #data format = 'title sensor_data memory_util cpu_util'
+    if topic not in data_dict:
+        data_dict[topic] = {data[0]: data[1], 'Memory': data[2], 'CPU': data[3]}
+    else:
+        keys = [data[0], 'Memory', 'CPU']
+        for i in range(3):
+            data_dict[topic][keys[i]] = data[i+1]
+
+
+def broker_loop():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.username_pw_set(username, password)
+    client.connect(broker_ip, broker_port_no, 60)
+
+    client.loop_forever()
 
 
 def define_axis():
@@ -24,8 +67,8 @@ def define_axis():
 
     axis = []
     size = len(data_dict)
-    for i in range(size*4):
-        axis.append(fig.add_subplot(size,4,i+1))
+    for i in range(size*3):
+        axis.append(fig.add_subplot(size,3,i+1))
 
     return axis
 
@@ -42,22 +85,22 @@ def _mov_avg(a1):
     return ma1
 
 
-def  plot_me():
+def plot_me():
     axes = define_axis()
-    a_m = -4
     a_c = -3
     a_i = -2
     a_e = -1
-    for k in plot_data:
-        a_m += 4
-        a_c += 4
-        a_i += 4
-        a_e += 4
-        plotter(ax=axes[a_m], data=plot_data[k]['mem'], key=k, name='Memory', col='m')
-        plotter(ax=axes[a_c], data=plot_data[k]['cpu'], key=k, name='CPU', col='c')
-        plotter(ax=axes[a_i], data=plot_data[k]['net_i'], key=k, name='NetI', col='g')
-        plotter(ax=axes[a_e], data=plot_data[k]['net_e'], key=k, name='NetE', col='r')
-    fig.suptitle('ShellMon Experiment')
+    hold_ax = [a_c, a_i, a_e]
+    for topic_ in data_dict:
+        a_c += 3
+        a_i += 3
+        a_e += 3
+        h = 0
+        for data_set in data_dict[topic_]:
+            plotter(ax=axes[hold_ax[h]], data=data_dict[topic_][data_set], key=topic_, name=data_set, col='c')
+            h+=1
+
+    fig.suptitle('IoT Sensor Network Smart City')
 
 
 def plotter(ax, data, key, name, col):
@@ -70,7 +113,6 @@ def plotter(ax, data, key, name, col):
         ax.set_ylabel('Client: {}'.format(key), rotation=0, fontsize=10, labelpad=30)
     ax.legend()
     plt.subplot(ax)
-
 
 
 def show_graphs():
