05472b0d895dd9620eafcce3b23da06f9c3a4006
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-21 08:33:03 +0000

    added broker setup

---
+++ broker_setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+clear
+echo "============ Setting up Your MQTT Broker ================="
+sleep 2
+apt update && apt upgrade -y
+apt install mosquitto mosquitto-clients -y
+touch /etc/mosquitto/passwd ; mosquitto_passwd -b /etc/mosquitto/passwd admin password
+echo -e 'allow_anonymous false \npassword_file /etc/mosquitto/passwd' > /etc/mosquitto/conf.d/default.conf
+/etc/init.d/mosquitto start
+/etc/init.d/mosquitto start
+apt install python3 -y
+apt install python3-pip -y
+pip3 install paho-mqtt
