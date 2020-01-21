3caed4ec313a18243b3a596a4b1804f4edb86e9f
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-21 16:37:47 +0000

    temp client done

---
+++ temp_client.py
@@ -0,0 +1,84 @@
+# This is a property of London South Bank University developed by EMEKA UGWUANYI
+
+import os
+import glob
+import time
+import paho.mqtt.client as mqtt
+import os
+import psutil
+import random as r
+
+'''
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+'''
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
+algo = psutil.Process()
+
+
+os.system('clear')
+print('-----------------------------------')
+print('Welcome to MQTT Publisher client')
+print('-----------------------------------')
+client = mqtt.Client()
+username = input('Username of Broker: ').strip()
+password = input('Password of Broker: ').strip()
+broker_ip = input("Broker's IP: ").strip()
+broker_port_no = 1883
+topic = 'iot/Temperature_server'
+print('-----------------------------------')
+
+
+client.username_pw_set(username, password)
+client.connect(broker_ip, broker_port_no, 60)
+
+
+def plot_cpu():
+    global prev_t
+
+    # get cpu
+    next_t = psutil.cpu_percent(percpu=False)
+    delta = abs(prev_t - next_t)
+    prev_t = next_t
+    return round(delta, 4)
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos + 2:]
+        temp_c = float(temp_string) / 1000.0
+
+        return temp_c
+
+
+def main():
+    while True:
+        #temp = read_temp()
+        temp = r.randrange(20)
+        if temp:
+            cpu = plot_cpu()
+            mem = round(algo.memory_percent(), 4)
+            message = f'Temperature {temp} {mem} {cpu}'
+            client.publish(topic, message)
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
